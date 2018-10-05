10b26c9bd88f60d9b17095596f6dc2b6413de6c9
Bendik Samseth <b.samseth@gmail.com>
2018-10-05 09:57:06 +0200

    Blacken tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -7,9 +7,7 @@ PLAYERS = [CROSS, NOUGHT]
 # Winning patterns encoded in bit patterns.
 # E.g. three in a row in the top row is
 #   448 = 0b111000000
-WINNING_PATTERNS = [448, 56, 7,    # Row
-                    292, 146, 73,  # Columns
-                    273, 84]       # Diagonals
+WINNING_PATTERNS = [448, 56, 7, 292, 146, 73, 273, 84]  # Row  # Columns  # Diagonals
 
 
 class Board(object):
@@ -68,10 +66,12 @@ class Board(object):
 
     def do_move(self, move):
         """Return a board where the suggested move has been made."""
-        b = Board(squares=self.squares[:],  # Copy squares.
-                  turn=self.next_player(),  # Swap player to move.
-                  depth=self.depth + 1)     # Increment depth.
-        b.squares[self.turn] |= move        # Apply move.
+        b = Board(
+            squares=self.squares[:],  # Copy squares.
+            turn=self.next_player(),  # Swap player to move.
+            depth=self.depth + 1,  # Increment depth.
+        )
+        b.squares[self.turn] |= move  # Apply move.
         return b
 
     @property
@@ -80,21 +80,23 @@ class Board(object):
 
     def __repr__(self):
         """Return string representation of the board."""
-        s = ''
+        s = ""
         for i in range(9):
             if self.squares[CROSS] & (1 << i):
-                s += 'X'
+                s += "X"
             elif self.squares[NOUGHT] & (1 << i):
-                s += 'O'
+                s += "O"
             else:
-                s += '-'
+                s += "-"
             if i % 3 < 2:
-                s += '|'
+                s += "|"
             elif i < 8:
-                s += '\n-----\n'
+                s += "\n-----\n"
         return s
 
+
 tb = dict()
+
 
 def search(board):
     """
@@ -112,12 +114,12 @@ def search(board):
 
     # Recursively explore the available moves, keeping
     # track of the best score and move to play.
-    bestScore, bestMove = -float('inf'), None
+    bestScore, bestMove = -float("inf"), None
     scoring = defaultdict(int)
     for move in board.moves():
 
         # v is score of position after the move.
-        v = - search(board.do_move(move))[0]
+        v = -search(board.do_move(move))[0]
 
         # New best move?
         if v > bestScore:
@@ -129,6 +131,7 @@ def search(board):
     absScore = bestScore if board.turn == CROSS else -bestScore
     tb[board.as_hash] = (absScore, scoring[bestScore])
     return bestScore, bestMove
+
 
 def bitboard_to_list(board):
     squares = []
@@ -142,15 +145,15 @@ def bitboard_to_list(board):
 
 if __name__ == "__main__":
     search(Board())
-    with open('tictactoe-data.csv', 'w') as csv:
-        print(('x{},'*9).format(*range(1, 10)), end='', file=csv)
-        print(('o{},'*9).format(*range(1, 10)), end='', file=csv)
-        print(('m{},'*9).format(*range(1, 10)), end='', file=csv)
-        print('turn,score', file=csv)
+    with open("tictactoe-data.csv", "w") as csv:
+        print(("x{}," * 9).format(*range(1, 10)), end="", file=csv)
+        print(("o{}," * 9).format(*range(1, 10)), end="", file=csv)
+        print(("m{}," * 9).format(*range(1, 10)), end="", file=csv)
+        print("turn,score", file=csv)
         for ((xs, os), turn), (score, moves) in tb.items():
             x, o, moves = map(bitboard_to_list, [xs, os, moves])
             x.extend(o)
             x.extend(moves)
             x.append(turn)
             x.append(score)
-            print(','.join(map(str, x)), file=csv)
+            print(",".join(map(str, x)), file=csv)
